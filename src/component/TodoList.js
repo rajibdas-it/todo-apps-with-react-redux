e2103e52693706d2e04105dc541036cfbbc39821
rajibdas-it <rajibrad@gmail.com>
2023-02-27 21:36:00 +0600

    filter by color done

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -5,7 +5,7 @@ import Todo from "./Todo";
 const TodoList = () => {
   const todos = useSelector((state) => state.todos);
   const filters = useSelector((state) => state.filters);
-  // console.log(todos);
+
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
       {todos
